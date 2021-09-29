e8aa1f39281e980c18d5ef6a37e2985f1dcae18c
hoodieman <19051147@brookes.ac.uk>
2021-09-29 08:43:08 +0100

    Initial Commit

---
+++ NoWaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoWaster
+//
+//  Created by 陳家豪 on 29/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NoWaster/NoWasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoWasterApp.swift
+//  NoWaster
+//
+//  Created by 陳家豪 on 29/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct NoWasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
